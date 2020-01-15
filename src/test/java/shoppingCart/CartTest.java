7ef7879acec1a19598c2ef0fd785e849acc963b6
mcheich <mcheich@gmail.com>
2020-01-15 18:36:17 -0500

    First Commit

---
+++ src/test/java/shoppingCart/CartTest.java
@@ -0,0 +1,14 @@
+package shoppingCart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
